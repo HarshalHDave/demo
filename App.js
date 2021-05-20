aa3a18a6fab36c2f1ca1ed3f212951766a8e3416
HarshalHDave <harshaldave74@gmail.com>
2021-05-20 23:33:30 +0530

    first push

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react - native';
+
+class HelloThere extends React.Component {
+  clickMe() {
+    Alert.alert("Hi!")
+  }
+  render() {
+    return (
+      <TouchableOpacity onPress={this.clickMe()}>
+        <View style={styles.box}>
+          <Text style={styles.message}>Hello {this.props.name}. Please click me.</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  box: {
+    borderColor: 'red',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    padding: 10,
+    width: 100,
+    height: 100
+  },
+  message: {
+    fontFamily: 'myCustomFont'
+  }
+});
+
+React.render(<HelloThere name="Component" />, document.getElementById('content'));
